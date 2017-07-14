72a762377e98e7d8e5ceb4dfe4ed2b68af5658b2
Takkar <www.shtakkar@gmail.com>
2017-07-14 23:47:47 +0530

    Added initial files

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # Handy-Highlight
+
+A very handy tool for highlighting the text & having fun - Inspired by [Medium](https://medium.com)!!
+
+# Quick links
+1. [Usage](#usage)
+2. [Demo](#demo)
+3. [API](#api)
+
+# Usage
+1. Install with `npm` or `Yarn`.
+
+    ```
+    npm install handy-highlight
+    ```
+    or
+    ```
+    yarn add handy-highlight
+    ```
+
+2. Add these `divs` to your markup.
+
+    ```html
+    <div id="cal1">&nbsp;</div>
+    <div id="cal2">&nbsp;</div>
+    <div id="handy-tooltip"></div>
+    ```
+
+3. Copy everything of `/highlight.css` into your stylesheet.
+
+4. Here comes the fun part. Should be used on `mouseup` function on `window` object.
+
+    ```javascript
+    import Highlight from 'handy-highlight'
+    window.onmouseup = function(){
+        Highlight({
+            onclick: function(){ console.log("Clicked the div!!") }
+        });
+    }
+    ```
+
+# Demo
+**Visit [this](https://codepen.io/Takkar/pen/qmrpeP) link for the demo!!**
+
+# API
+```javascript
+Highlight(options:Object)
+options = {
+    extraTop:Number,
+    extraLeft:Number,
+    text:String,
+    onclick:Function
+}
+```
+
+<dl>
+  <dt>extraTop:</dt>
+  <dd>Add some extra top to the div.</dd>
+
+  <dt>extraLeft:</dt>
+  <dd>Add some extra left to the div.</dd>
+
+  <dt>text:</dt>
+  <dd>Text of the div. Ignore this property if you want div's text to be the copied text.</dd>
+
+  <dt>onclick:</dt>
+  <dd>Function to be executed when clicked on the div.</dd>
+</dl>
+
+**Thnx for reading!!**
---
+++ index.js
@@ -0,0 +1,55 @@
+var ele = document.getElementById('handy-tooltip');
+
+var getSelectedText = function(){
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+var HandyHighlight = function(options){
+
+    var 
+        defaults = { 
+            extraTop: null,
+            extraLeft: null,
+            text: null,
+            onclick: function(){ return; }
+        },
+        settings = Object.assign({}, defaults, options),
+        sel = window.getSelection(),
+        rel1 = document.createRange(),
+        rel2= document.createRange(),
+        text = (settings.text == null) ? getSelectedText() : settings.text;
+    
+    rel1.selectNode(document.getElementById('cal1'));
+    rel2.selectNode(document.getElementById('cal2'));
+
+    if (text != "") {
+
+        var r = sel.getRangeAt(0).getBoundingClientRect(),
+            rb1 = rel1.getBoundingClientRect(),
+            rb2 = rel2.getBoundingClientRect(),
+            width = (r.width > 1300) ? 1300 : r.width;
+    
+        ele.style.top = (r.bottom - rb2.top)*100/(rb1.top-rb2.top) + 5 + settings.extraTop + 'px'; 
+        ele.style.left = (r.left - rb2.left)*100/(rb1.left-rb2.left) + settings.extraLeft + 'px'; 
+
+        ele.style.width = width+"px";
+        ele.innerHTML = text;
+        ele.style.display = 'block';   
+
+        ele.onclick = function(){
+            settings.onclick();
+        } 
+    
+    } else if(text == ""){
+        ele.style.display = "none";
+    }
+
+}
+
+module.exports = HandyHighlight;
